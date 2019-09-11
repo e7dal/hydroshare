b232fbe3a04a17ed29aea63ba62163f7eade6b9f
Mark OBrien <mark_noreply@noreply.com>
2019-09-11 12:38:43 -0400

    Update icons

--- hs_core/static/js/search.js
+++ hs_core/static/js/search.js
@@ -1,5 +1,4 @@
-//https://jsfiddle.net/Tertia/vbyon64p/6/?utm_source=website&utm_medium=embed&utm_campaign=vbyon64p
-Vue.component('demo-grid', {
+Vue.component('discover-grid', {
     template: '#grid-template',
     props: {
         heroes: Array,
@@ -7,10 +6,10 @@ Vue.component('demo-grid', {
         filterKey: String
     },
     data: function () {
-        var sortOrders = {};
+        let sortOrders = {};
         this.columns.forEach(function (key) {
           sortOrders[key] = 1
-        })
+        });
         return {
             sortKey: '',
             sortOrders: sortOrders
@@ -18,10 +17,10 @@ Vue.component('demo-grid', {
     },
     computed: {
         filteredHeroes: function () {
-            var sortKey = this.sortKey;
-            var filterKey = this.filterKey && this.filterKey.toLowerCase();
-            var order = this.sortOrders[sortKey] || 1;
-            var heroes = this.heroes;
+            let sortKey = this.sortKey;
+            let filterKey = this.filterKey && this.filterKey.toLowerCase();
+            let order = this.sortOrders[sortKey] || 1;
+            let heroes = this.heroes;
             if (filterKey) {
                 heroes = heroes.filter(function (row) {
                     return Object.keys(row).some(function (key) {
@@ -48,32 +47,60 @@ Vue.component('demo-grid', {
         sortBy: function (key) {
             this.sortKey = key;
             this.sortOrders[key] = this.sortOrders[key] * -1
-        },
-        searchClick: function () {
-            console.log('asdf')
         }
     }
 });
 
-var demo = new Vue({
-    el: '#demo',
+let DiscoverApp = new Vue({
+    el: '#discover-search',
     data: {
         searchQuery: '',
-        gridColumns: ['name', 'author'],
+        gridColumns: ['name', 'type', 'author'],
         gridData: [
             {
                 name: 'SSCZO - Flux Tower, Meteorology - Flux Tower Transect, Soaproot Saddle (2009-2016)',
+                type: 'GenericResource',
                 author: 'Kim Dailey'
             },
             {
                 name: 'LCZO - Soil Biogeochemistry - landscape-scale soil biogeochemistry and enzymes - Bisley (2012)',
+                type: 'CollectionResource',
                 author: 'Roger Downs'
             },
-            {name: 'X-CZO - Flux Tower - AmeriFlux Network data - National (2007-2018)', author: 'Jill Hickey'},
+            {
+                name: 'X-CZO - Flux Tower - AmeriFlux Network data - National (2007-2018)',
+                type: 'GenericResource',
+                author: 'Jill Hickey'
+            },
             {
                 name: 'BCCZO - Precipitation - B1 Historical Precipitation Site (B1_Hist_Precip) - B1 Historical Site (1952-1964)',
+                type: 'GenericResource',
                 author: 'Ed Brown'
             }
         ]
+    },
+    methods: {
+        searchClick: function (csrf_token, ev, topic) {
+            let formData = new FormData();
+            // Vue.set(topic, 'edit', false);
+            // TODO all the querystring params
+            formData.append("csrfmiddlewaretoken", csrf_token);
+            formData.append("id", topic.val[0].toString());
+            formData.append("name", topic.val[1].toString());
+            formData.append("action", "UPDATE");
+            $.ajax({
+                type: "POST",
+                data: formData,
+                processData: false,
+                contentType: false,
+                url: "/the_endpoint_for_discover/",
+                success: function (response) {
+                    // do nothing
+                },
+                error: function (response) {
+                    console.log(response.responseText);
+                }
+            });
+        }
     }
 });
